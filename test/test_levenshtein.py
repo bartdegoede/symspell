7fd964fe9e6185f0c467ced3b75515d89fab36d3
Bart de Goede <bart@scribd.com>
2020-07-21 22:36:56 -0700

    Added some tests with different sequences

--- test/test_levenshtein.py
+++ test/test_levenshtein.py
@@ -2,17 +2,21 @@ from symspell import edit_distance
 
 def test_substitution():
     assert edit_distance.damerau_levenshtein('bulthaup', 'bolthaup') == 1
+    assert edit_distance.damerau_levenshtein('bla', ['b', 'l', 'b']) == 1
     assert edit_distance.damerau_levenshtein('bulthaup', 'bolthaip') == 2
     assert edit_distance.damerau_levenshtein('bulthaup', '') == 8
 
 def test_addition():
     assert edit_distance.damerau_levenshtein('bulthaup', 'bulthaupi') == 1
+    assert edit_distance.damerau_levenshtein('bla', ['b', 'l', 'a', 'b']) == 1
     assert edit_distance.damerau_levenshtein('bulthaup', 'bulthaupij') == 2
 
 def test_deletion():
     assert edit_distance.damerau_levenshtein('bulthaup', 'bulthap') == 1
+    assert edit_distance.damerau_levenshtein('bla', ['b', 'l']) == 1
     assert edit_distance.damerau_levenshtein('bulthaup', 'bulthp') == 2
 
 def test_transposition():
     assert edit_distance.damerau_levenshtein('bulthaup', 'bulthuap') == 1
+    assert edit_distance.damerau_levenshtein('bla', ['b', 'a', 'l']) == 1
     assert edit_distance.damerau_levenshtein('bulthaup', 'bultuhap') == 2
