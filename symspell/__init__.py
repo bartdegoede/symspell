4a0cd544d99e538aef9a35a9cad1d07f6a3c8731
Bart de Goede <bart@scribd.com>
2020-07-23 00:22:51 -0700

    Add some English words and a method to load create the dictionary

--- symspell/__init__.py
+++ symspell/__init__.py
@@ -1,1 +1,6 @@
+import logging
+
+logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s',
+                    level=logging.INFO)
+
 from .symspell import Symspell
--- symspell/symspell.py
+++ symspell/symspell.py
@@ -1,5 +1,8 @@
+import logging
+import re
+
 class Symspell(object):
-    def __init__(self, tokenizer=lambda x: x.split()):
+    def __init__(self, tokenizer=lambda x: re.findall('[a-z]+', x)):
         self.dictionary = {}
         self.longest_word_length = 0
         self.tokenizer = tokenizer
@@ -48,3 +51,50 @@ class Symspell(object):
                     self.dictionary[deletion] = ([word], 0)
 
         return new_word_added
+
+    def create_dictionary(self, fname):
+        """
+        Create a dictionary from a file with text. It will read a file line by
+        line, lower case it, and tokenize the words using self.tokenizer
+        """
+        total_words = 0
+        unique_words = 0
+
+        with open(fname, 'r') as f:
+            for line in f:
+                for word in self.tokenizer(line.lower()):
+                    total_words += 1
+                    if self.create_dictionary_entry(word):
+                        unique_words += 1
+                        if unique_words % 1000 == 0:
+                            logging.debug(f'Processed {unique_words} unique words')
+        logging.info(f'Total words processed: {total_words}')
+        logging.info(f'Total unique words in corpus: {unique_words}')
+        logging.info(f'Dictionary size (corpus words and deletions): {len(self.dictionary)}')
+        logging.info(f'  edit distance for deletions: 2')
+        logging.info(f'  length of longest word in corpus: {self.longest_word_length}')
+
+    # def suggest(self, query):
+    #     """
+    #     Return a list of suggested corrections for potentially incorrectly spelled words
+    #     """
+    #     if len(query) - self.longest_word_length > 2:
+    #         logging.warning('There are no items in the dictionary within the maximum edit distance')
+    #         return []
+
+    #     suggestions = {}
+    #     queue = [query]
+
+    #     while len(queue) > 0:
+    #         q = queue.pop()
+
+    #         if q in self.dictionary and q not in suggestions:
+    #             if self.dictionary[q][1] > 1:
+    #                 # The query is in the dictionary, and it is a word from the
+    #                 # correctly spelled corpus (>1). It is also not in the
+    #                 # suggestions list yet, so add it with the frequency in the
+    #                 # corpus and the edit distance
+    #                 suggestions[q] = (self.dictionary[q][1], len(query) - len(q))
+    #                 # if len(query) == len(q):
+    #                 #     break
+    #                 if (len(query) - len(q)) < min
